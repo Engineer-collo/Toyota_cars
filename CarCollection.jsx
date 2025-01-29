466cf592c9c18b3ab84c5f78365a132abf2dd565
Njire Njeri <njirenjeri@gmail.com>
2025-01-29 14:07:28 +0300

    added styling on car collection display

--- CarCollection.jsx
+++ CarCollection.jsx
@@ -1,18 +1,17 @@
 function CarCollection({data}) {
   return (
-    <div>
+    <div className='main'>
     {data.map((car) => {
         return (
-          <ul className='main'>
+          <ul >
           <li className='car-container' key={car.id}>
             
-            <h1>Car name:{car.name}</h1>
+            <h1>{car.name}</h1>
             <img src={ car.picture}  alt={car.make}  />
             <h5>Ksh.{car.price}</h5>
-            <h2>Car model:{car.model}</h2>
-            <h3>Make:{car.year}</h3>
-            <h4>Fuel type:{car.fuel}</h4>
-            <h4>Displacement:{car.displacement}</h4>
+            <h2>model: {car.model} </h2>
+            <h3>Year: {car.year}</h3>
+            <h4>Fuel type: {car.fuel} Displacement:{car.displacement} </h4>
             <button className="buyCar">Buy car</button>
             
           </li>
---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <form action="">
+            <input 
+                type="text" 
+                id='search-input'            
+            />
+        </form>
+    </div>
+  )
+}
+
+export default Navbar
