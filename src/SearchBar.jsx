c3f96b78fddf1869a0b172315275979352a74683
kelvin_kiprop <kelkiprop@gmail.com>
2025-02-01 23:34:58 +0300

    updates

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function SearchBar({ searchQuery, setSearchQuery}) {   
-    return( 
-        <>           
-                <input 
-                    type="text" 
-                    id="search-input" 
-                    placeholder="Search" 
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    autoComplete="off"
-                />
-        </>
-    )
+function SearchBar({ searchQuery, setSearchQuery }) {
+  return (
+    <>
+      <input
+        type="text"
+        id="search-input"
+        placeholder="Search"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        autoComplete="off"
+      />
+    </>
+  );
 }
 
 export default SearchBar;
