4efa9d103ac5af3b651bd429d77f1916e2fec071
rayjon_karuga <karugaray575s@gmail.com>
2025-02-01 19:37:00 +0300

    changed styling

---
+++ src/assets/logo.png/logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './assets/logo.png'; // Adjust the path to where you stored the logo image
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={logo} alt="Logo" style={{ width: '150px', height: 'auto' }} />
+    </div>
+  );
+};
+
+export default Logo;
+rdfg 
