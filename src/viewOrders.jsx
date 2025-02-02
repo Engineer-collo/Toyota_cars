54a3857760775f04d25fdca0b49ff80c015581a9
Engineer-collo <collinslikhomba@gmail.com>
2025-02-02 23:03:48 +0300

    Update viewOrders.jsx

--- src/viewOrders.jsx
+++ src/viewOrders.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import "./ViewOrders.css"
 const ViewOrders = () => {
@@ -9,82 +10,59 @@ const ViewOrders = () => {
    .then((response) => response.json())
     .then((data) => setOrders(data))
   }, []);
+  const deleteOrder = (orderId) => {
+    const updatedOrders = orders.filter((order) => order.id !== orderId);
+    setOrders(updatedOrders);
 
-  // function to delete an order
-  const handleDelete = (id) =>{
-    fetch(`http://localhost:8001/orders/${id}`,{
-      method: 'DELETE',
-    })
-    .then((response) => {
-      if(response.ok){
-        setOrders(orders.filter(order => order.id !== id))
-      }
-    })
-    .catch((error) => console.error('error deleting', error))
-  }
-
+    fetch(`http://localhost:8001/orders/${orderId}`, {
+        method: 'DELETE',
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log(`Order ${orderId} deleted successfully`);
+          } else {
+            console.error('Failed to delete order');
+          }
+        })
+        .catch((error) => console.error('Error:', error));
+    };
+    
   return (
-    <div className='orders'>
+    <div>
       <h1>Admin Dashboard</h1>
       
-<<<<<<< HEAD
-      <h2>Car Orders</h2>
+      <h2>Cars List</h2>
       <table>
         <thead>
           <tr>
         
-            <th>Order ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Phone Number</th>
-            <th>Customer Email Adress</th>
-            <th>Shipping Adress</th>
-            <th>Car Model</th>
-            <th>Car Price</th>
-      
-            
-=======
-      <h2>orders</h2>
-      <table>
-        <thead>
-          <tr>        
             <th>OrderID</th>
             <th>First Name</th>
             <th>Last Name</th>
             <th>Phone Number</th>
-            <th>Email</th>
-            <th>Shipping Address</th>
+            <th>Email Adress</th>
+            <th>Shipping Adress</th>
             <th>Make</th>
             <th>Model</th>
             <th>Price</th>
             <th>Available</th>
->>>>>>> b0b1465b10e4ace5944c8361503bea393c12d594
           </tr>
         </thead>
         <tbody>
-          {orders.length > 0 ? (orders.map((order) => (
+          {orders.map((order) => (
             <tr key={order.id}>
               <td>{order.id}</td>
               <td>{order.firstname}</td>
               <td>{order.lastname}</td>
               <td>{order.phoneNumber}</td>
               <td>{order.email}</td>
-<<<<<<< HEAD
-              <td>{order.address}</td> 
+              <td>{order.adress}</td>
+              <td>{order.car.make}</td>
               <td>{order.car.model}</td>
               <td>{order.car.price}</td>
-              
-=======
-              <td>{order.address}</td>
-              <td>{order.car.year}</td>
-              <td>{order.car.model}</td>
-              <td>{order.car.price}</td>
-              <td><button onClick={()=>handleDelete(order.id)}>delete</button></td>
->>>>>>> b0b1465b10e4ace5944c8361503bea393c12d594
+              <td>{order.available ? "Yes" : "No"}</td>
             </tr>
-          ))):(
-            <td>No pending Orders</td>
-          )}
+          ))}
         </tbody>
       </table>
       </div> 
