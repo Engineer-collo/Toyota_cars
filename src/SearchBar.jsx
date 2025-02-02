8e29f653280fbdee172e5f82e01dc6def7254be6
Engineer-collo <collinslikhomba@gmail.com>
2025-02-02 18:14:06 +0300

    Update SearchBar.jsx

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,1 +1,19 @@
 
+import { useState } from "react"
+
+function SearchBar({ searchQuery, setSearchQuery}) {   
+    return( 
+        <>           
+                <input 
+                    type="text" 
+                    id="search-input" 
+                    placeholder="Search" 
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    autoComplete="off"
+                />
+        </>
+    )
+}
+
+export default SearchBar;
