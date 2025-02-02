740f08de1ea4e3cc43d2c9c419e7d4c5c79eec57
Engineer-collo <collinslikhomba@gmail.com>
2025-02-02 18:05:51 +0300

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,1 +1,37 @@
+import { Link } from "react-router-dom";
+function Footer() {
+  return (
+    <footer className="footer">        
+      <div className="social-media">
+        <h3>Follow us.</h3> 
+            <a href="#" target="_blank" rel="noreferrer">Facebook</a> <br/>
+            <a href="#" target="_blank" rel="noreferrer">Twitter</a>  <br/>
+            <a href="#" target="_blank" rel="noreferrer">Instagram  </a>
+          </div>
+ 
 
+      <p className="footer-text">&copy; 2025, All Rights Reserved. <br/>
+      This website was created for educational purposes by:  <br/>
+      1. Julius  <br/>  
+      2. Collins <br/> 
+      3. Rayjon  <br/>
+      4. Khadar  <br/>
+      5. Kelvin  <br/>
+      6. Hosea
+    </p> 
+
+
+     
+    
+  <ul>
+    <li>
+    <Link to="/comments">Comments</Link>
+    </li>
+  </ul>
+
+    </footer>
+
+  );
+}
+
+export default Footer;
